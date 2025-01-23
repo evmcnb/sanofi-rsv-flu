55fdae49a6683dff88a42f859b7285e8b3ca2d50
evmcnb <12015958+evmcnb@users.noreply.github.com>
2025-01-23 16:43:05 +0000

    jhg

--- R/HK_data.R
+++ R/HK_data.R
@@ -1,4 +1,4 @@
 
 
 flu_hk_data <- read.csv("csv/HK/influenza.csv") %>%
-  select(Week, Year, H1, H3, B, AandB, starts_with("Adm_0_5"), starts_with("SevereCase"))
+  select(Week, Year, H1, H3, B, AandB, starts_with("Adm_"), starts_with("SevereCase"))
