1e337022ec13e5e597a0af1132dccdb6911d745f
kayelan <129391403+kayelan@users.noreply.github.com>
2025-01-24 12:20:46 +0000

    Create Japan_data.R

---
+++ R/Japan_data.R
@@ -0,0 +1,16 @@
+setwd("C://Users//lukeb//Downloads//LSHTM//TERM 2//Data Challenge//github//sanofi-rsv-flu")
+
+# directory path for datasets
+path <- "csv/Japan/"
+
+# create file names for each year
+years <- 2015:2024
+file_names <- paste0(path, years, ".csv")
+
+# read all datasets,create a Year column and combine
+Japan_data <- do.call(rbind, lapply(file_names, function(file) {
+  data <- read.csv(file)
+  data$Year <- gsub("^.*/|\\.csv$", "", file) # extract year from filename
+  return(data)
+}))
+
