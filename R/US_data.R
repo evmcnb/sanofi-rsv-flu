08496ff15d17d09f2da938ae901ae29ff70a2b11
evmcnb <12015958+evmcnb@users.noreply.github.com>
2025-01-24 15:23:17 +0000

    pretty plots

--- R/US_data.R
+++ R/US_data.R
@@ -1,19 +1,30 @@
 
 
 flu_us_data <- read_excel("csv/US/influenza.xlsx") %>%
-  set_names(gsub("\\s+", "_", names(flu_us_data))) %>% 
-  filter(VIRUS_TYPE_CATEGORY == "Overall") %>% 
-  group_by(YEAR...4, WEEK, AGE_CATEGORY) %>% 
+  set_names(gsub("\\s+", "_", names(.))) %>% 
+  filter(VIRUS_TYPE_CATEGORY == "Overall") %>%
+  mutate(
+    age_group = case_when(
+      AGE_CATEGORY %in% c("0-< 1 yr", "< 18") ~ "Infant",                 # Ages 0-<1 or <18
+      AGE_CATEGORY %in% c("0-4 yr", "1-4 yr", "5-11  yr", "5-17 yr") ~ "Adolescent", # Ages 1-17
+      AGE_CATEGORY %in% c("18-29 yr", "18-49 yr", "30-39 yr", "40-49 yr", "50-64 yr") ~ "Adult", # Ages 18-64
+      AGE_CATEGORY %in% c("65-74 yr", "75-84 yr", ">= 65 yr", ">= 75", ">= 85") ~ "Elderly", # Ages 65+
+      AGE_CATEGORY == "Overall" ~ "All",                                     # Keep "Overall" as a separate category
+      TRUE ~ NA_character_                                                    # Mark unexpected values as NA
+    ),
+    age_group = factor(age_group, levels = c("Infant", "Adolescent", "Adult", "Elderly", "All"))
+  ) %>%
+  group_by(YEAR...4, WEEK, age_group) %>% 
   summarise(
     weekly_rate = sum(WEEKLY_RATE),
     .groups = "drop"
   ) %>%
-  arrange(YEAR...4, WEEK)
+  arrange(YEAR...4, WEEK) %>% 
+  view()
 
-rsv_us_data <- read_csv("csv/US/RSV.csv") 
 
-rsv_us_data <- rsv_us_data %>%
-  set_names(gsub("\\s+", "_", names(rsv_us_data))) %>%
+rsv_us_data <- read_csv("csv/US/RSV.csv")  %>%
+  set_names(gsub("\\s+", "_", names(.))) %>%
   mutate(
     year = year(Week_ending_date),
     week_number = week(Week_ending_date)
@@ -26,4 +37,5 @@ rsv_us_data <- rsv_us_data %>%
     .groups = "drop"
   ) %>%
   arrange(year, week_number) %>%
-  distinct()
+  distinct() %>% 
+  view()
--- R/Uk_data.R
+++ R/Uk_data.R
@@ -2,13 +2,49 @@
 # Flu data ----------------------------------------------------------------
 
 flu_uk_data <- read.csv("csv/UK/influenza.csv") %>%
-  mutate(year = factor(year)) %>% 
+  mutate(year = factor(year)) %>%
+  mutate(month = factor(month(date, label = TRUE, abbr = TRUE))) %>%
   select(sex, age, year, month, epiweek, date, metric_value)
 
 rsv_uk_data <- read.csv("csv/UK/RSV.csv") %>%
   mutate(year = factor(year)) %>% 
+  mutate(month = factor(month(date, label = TRUE, abbr = TRUE))) %>%
   select(sex, age, year, month, epiweek, date, metric_value)
 
+
+
+
+uk_data <- flu_uk_data %>% 
+  mutate(flu_r10k = metric_value / 10,
+         country = "United Kingdom",
+         country_code = "GBR") %>%
+  full_join(rsv_uk_data, by= c("age", "year","month", "epiweek", "date")) %>% 
+  mutate(rsv_r10k = metric_value.y / 10) %>%
+  mutate(
+    age_group = case_when(
+      age %in% c("00-04") ~ "infant",                 # Ages 0-4
+      age %in% c("05-14") ~ "adolescent",             # Ages 5-14
+      age %in% c("15-44", "45-54", "55-64") ~ "adult", # Ages 15-64
+      age %in% c("65-74", "75-84", "75+", "85", "85+") ~ "elderly", # Ages 65+
+      age == "all" ~ "all",                           # Keep "all" as a separate category
+      TRUE ~ NA_character_                            # Mark unexpected values as NA
+    ),
+    age_group = factor(age_group, levels = c("infant", "adolescent", "adult", "elderly", "all"))
+  ) %>%
+  group_by(country, country_code, month, year, epiweek, age_group) %>% 
+  summarize(
+    flu_r10k = sum(flu_r10k, na.rm = TRUE),
+    rsv_r10k = sum(rsv_r10k, na.rm = TRUE),
+    .groups = "drop"
+  ) %>% 
+  select(country, country_code, year, epiweek, month, age_group, flu_r10k, rsv_r10k) %>% 
+  view()
+
+
+
+# Graphics --------------------------------------------------------------------
+
+# Bar graph over all the weeks 
 
 flu_uk_data %>%
   filter(age == "all") %>%  # Filter before grouping for efficiency
@@ -30,6 +66,7 @@ flu_uk_data %>%
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# Line graph of years coloured by the month
 flu_uk_data %>%
   filter(age == "all") %>%  # Filter for all ages and dates from 2015 onwards
   group_by(date) %>%  # Ensure grouping by date
@@ -52,5 +89,106 @@ flu_uk_data %>%
     axis.text.x = element_text(angle = 45, hjust = 1),
     legend.position = "bottom"
   )
+
+
+library(ggplot2)
+library(viridis)
+library(hrbrthemes)
+library(ggridges)
+
+# Density ridge plot - may need to center the data as hard to see trends
+flu_uk_data %>% 
+  filter(age == "all") %>% 
+  group_by(epiweek, year) %>%
+  summarise(cases = sum(metric_value)) %>%
+  ggplot(aes(x = epiweek, y = factor(year), height = cases, fill = factor(year))) +
+  geom_density_ridges(stat = "identity", scale = 1, rel_min_height = 0.01) +
+  labs(title = 'Flu Cases by Year and Week', x = 'Epi Week', y = 'Year') +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8),
+    axis.text.x = element_text(angle = 45, hjust = 1)  # Rotates the x-axis labels for better readability
+  )
+
+# Bar graphcs - cases per month
+flu_uk_data %>%
+  group_by(year, month) %>%
+  summarise(total_cases = sum(metric_value)) %>%
+  ggplot(aes(x = month, y = total_cases, fill = factor(year))) +
+  geom_col(position = "dodge") +
+  labs(title = 'UK Influenza Data', x = 'Year') +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  )
+
+# Also might need centering - good thing about the polar plots this isnt needed. year 7 = 2021
+flu_uk_data %>%
+  filter(age == "all") %>%
+  mutate(is_covid = if_else(as.numeric(year) < 7, 0, 1),
+         is_covid = factor(is_covid, labels = c("Before Lockdown", "After Lockdown"))) %>% 
+  group_by(epiweek, is_covid) %>%
+  summarise(cases = sum(metric_value)) %>% 
+  ggplot(aes(x = epiweek, y = cases, color = factor(is_covid))) +
+  geom_line() +
+  labs(title = 'UK Influenza Data', x = 'Week') +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  )
+
+# Same as above but in polar coordinates. Very clear plot - defo final
+flu_uk_data %>%
+  filter(age == "all") %>%
+  mutate(is_covid = if_else(as.numeric(year) < 7, 0, 1),
+         is_covid = factor(is_covid, labels = c("Before Lockdown", "After Lockdown"))) %>%
+  group_by(epiweek, is_covid) %>%
+  summarise(cases = sum(metric_value)) %>%
+  ggplot(aes(x = epiweek, y = cases, color = factor(is_covid))) +
+  geom_line(size = 1) +
+  labs(title = 'UK Influenza Data', subtitle = "Before Lockdown is defined as any data prior to 2021", x = 'Week', y = "Cumulative Hosptialization Rate per 10,000") +
+  theme_fivethirtyeight() + 
+  theme(
+    axis.title = element_text(),
+    legend.position = "bottom",
+    axis.ticks.y = element_line(),
+    axis.line.y.left = element_line(),
+    legend.title = element_blank(),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8),
+    axis.text.y = element_text()
+  ) +
+  coord_polar(theta = "x")
+
+ggsave(
+  filename = "plots/Polar/flu_uk_polar_plot.png",  # Name of the file (you can change the extension to .jpg, .pdf, etc.)
+  plot = last_plot(),  # This refers to the last plot generated
+  width = 6,  # Width of the plot (in inches)
+  height = 6,  # Height of the plot (in inches)
+  dpi = 300  # Resolution (dots per inch) - 300 is good for print quality
+)
+
+
+# Similar to above but not aggregated 
+flu_uk_data %>%
+  filter(age == "all") %>%
+  group_by(epiweek, year) %>%
+  summarise(cases = sum(metric_value)) %>%
+  ggplot(aes(x = epiweek, y = cases, color = factor(year))) +
+  geom_line() +
+  labs(title = 'UK Influenza Data', x = 'Week') +
+  theme(
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  ) +
+  coord_polar(theta = "x")
 
 
