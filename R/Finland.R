e7ea380a433809c1faf950dbd84914c3c3c29bb4
kayelan <129391403+kayelan@users.noreply.github.com>
2025-01-29 19:43:48 +0000

    Update Finland.R

--- R/Finland.R
+++ R/Finland.R
@@ -1,12 +1,13 @@
 # need this for separate
 library(tidyr)
 
-# split Time column accordingly
+# read in with delimited and split Time column into months and years
 cases_201019 <- read.csv("csv/Finland/fact_ttr_cases 2010 -2019.csv", sep = ";") |>
   separate(Time, into = c("Month", "Year"), sep = " ")
 cases_202025 <- read.csv("csv/Finland/fact_ttr_cases 2020-2025.csv", sep = ";") |>
   separate(Time, into = c("Month", "Year"), sep = " ")
 
+# rename columns for ease
 colnames(cases_201019) <- c("Month", "Year", "Group", "Cases")
 colnames(cases_202025) <- c("Month", "Year", "Group", "Cases")
 
@@ -25,11 +26,11 @@ library(scales)
 # convert month and date to combined format
 Finland_data <- Finland_data |> mutate(Date = dmy(paste("01", Month, Year)))
 
-# plot the data
+# plot the data using consistent aesthetic
 ggplot(Finland_data, aes(x = Date, y = Cases, color = Group)) +
   geom_line(size = 1) +
-  labs(title = "Seasonality of RSV and Influenza in Finland 2010-2025", 
-       x = "Date", 
+  labs(title = "Seasonality of RSV and Influenza in Finland 2015-2025", 
+       x = "Year", 
        y = "Cases") +
   theme_fivethirtyeight() +
   theme(
@@ -44,7 +45,7 @@ ggplot(Finland_data, aes(x = Date, y = Cases, color = Group)) +
   ) +
   scale_x_date(breaks = seq(min(Finland_data$Date), max(Finland_data$Date), by = "1 year"),
                labels = date_format("%Y"),
-               limits = c(as.Date("2014-01-01"), max(Finland_data$Date))) +
+               limits = c(as.Date("2015-01-01"), max(Finland_data$Date))) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # work on difference in peaks. adjust data to allow for peaks being on either side of NYE
@@ -66,12 +67,11 @@ get_peak_month <- function(data, disease) {
 flu_peaks <- get_peak_month(Finland_data, "Influenza")
 rsv_peaks <- get_peak_month(Finland_data, "RSV")
 
-# REWRITE THIS
-# Merge peak data
+# merge peak data
 peak_diff <- inner_join(flu_peaks, rsv_peaks, by = "Epi_year") %>%
   mutate(Difference = as.numeric(difftime(Influenza_Peak, RSV_Peak, units = "days")))  # Compute difference in days
 
-# Plot difference over epidemic seasons - issue in 2020 from lack of data/cases
+# plot difference over epidemic seasons - issue in 2020 from lack of data/cases
 # improve plot to indicate direction (+/-) clearly
 ggplot(peak_diff, aes(x = Epi_year, y = Difference)) +
   geom_line() +
