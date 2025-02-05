89ce842780ccd42165efbee83e60e8b0294b70e5
kayelan <129391403+kayelan@users.noreply.github.com>
2025-02-05 15:41:45 +0000

    Update ccf_plots.R

--- R/ccf_plots.R
+++ R/ccf_plots.R
@@ -9,7 +9,7 @@ library(readxl)
 library(ggthemes)
 library(ggplot2)
 library(dplyr)
-library(zoo) # library for rolling average
+library(readr)
 
 
 # retrieve the main dataset with filters on years and disease
@@ -28,9 +28,7 @@ flu_dataset <- read_csv("csv/main_dataset.csv",
                         )) %>%
   select(country, year, week, disease, age, metric) %>%
   filter(disease == "Influenza") %>%
-  filter(year > 2018) %>% # UPDATE later to include more pre-COVID years
-  filter(year < 2025) %>% # avoid incomplete year issue
-  filter(week < 53) %>% # avoid issue of 52 vs 53 weeks - use imputing instead as week 53 may have cases!
+  filter(year >= 2017) %>%
   select(-disease)
 
 # collapse into weekly summaries by summing over age groups
@@ -38,3 +36,28 @@ flu_dataset <- read_csv("csv/main_dataset.csv",
 flu_dataset <- flu_dataset %>%
   group_by(country, year, week) %>%
   summarise(cases = sum(metric, na.rm = TRUE), .groups = "drop")
+
+# hemisphere information for included countries
+inc_countries <- unique(flu_dataset$country)
+southern_hemisphere <- c(
+  "Argentina", "Australia", "Bolivia", "Botswana", "Brazil", "Chile", "Colombia",
+  "Ecuador", "Eswatini", "Fiji", "Lesotho", "Madagascar", "Malawi", "Mauritius",
+  "Mozambique", "Namibia", "New Zealand", "Paraguay", "Peru", "Papua New Guinea",
+  "Rwanda", "Samoa", "Solomon Islands", "South Africa", "Tanzania", "Timor-Leste",
+  "Tonga", "Uruguay", "Vanuatu", "Zambia", "Zimbabwe"
+)
+hemisphere <- setNames(
+  ifelse(inc_countries %in% southern_hemisphere, "S", "N"), 
+  inc_countries
+)
+
+# add epi_week which is the shifted week based on hemisphere
+flu_dataset <- flu_dataset %>%
+  mutate(epi_week = case_when(
+    hemisphere[country] == "N" ~ (week + 26) %% 52,  # Shift by 26 weeks if in Northern Hemisphere
+    TRUE ~ week  # Keep the same for Southern Hemisphere
+  ))
+
+# grouped dataset based on covid
+
+flu_covid <- 
